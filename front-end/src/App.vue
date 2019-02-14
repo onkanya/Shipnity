939ecdce3fd6d44a426c2f6a681bb1eb3f41d975
onkanya <onkanyalearning@gmail.com>
2019-02-14 21:26:58 +0700

    first commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <el-container>
+        <el-header class="m-navbar">
+            <h1>Shipnity</h1>
+        </el-header>
+        <el-main class="m-content">
+            <el-row>
+                ราคาแพ็คเก็จ  (เลือกให้ดีที่สุดฉบับธุรกิจคุณ)
+            </el-row>
+            <el-row>
+                * ตัวแทนจำหน่ายของร้าน สามารถมี user ของตัวเองเพื่อเข้ามาสร้างออร์เดอร์, เช็คสต็อก, ดูประวัติ, ยอดขายของตัวเองได้
+            </el-row>
+            <el-row>
+                <el-col class="m-subcontent">
+                    <el-row :gutter="20" justify="space-between" align="middle">
+                        <!-- Image -->
+                        <el-col :xs="24" :sm="12" :md="12" :lg="12">
+                            <div class="image">
+                                Image
+                            </div>
+                        </el-col>
+                        <!-- Text -->
+                        <el-col :xs="24" :sm="6" :md="6" :lg="6" class="m-side-detail">
+                            <el-row>
+                                <el-col :span="24">
+                                    สมัครวันนี้ ฟรีทันที 50 ออร์เดอร์
+                                </el-col>
+                            </el-row>                            
+                            <el-row>
+                                <el-col :span="24">
+                                    - ใช้งานฟรีตลอดไป
+                                </el-col>
+                            </el-row>                            
+                            <el-row>
+                                <el-col :span="24">
+                                    - ไม่จำกัดฟีเจอร์
+                                </el-col>
+                            </el-row>                            
+                            <el-row>
+                                <el-col :span="24">
+                                    ขายของครบเครื่องทุกฟังก์ชัน
+                                </el-col>
+                            </el-row>                            
+                        </el-col>
+                        <el-col :xs="24" :sm="6" :md="6" :lg="6">
+                            <el-button type="primary">สมัครใช้ระบบฟรีตลอดไป</el-button>
+                        </el-col>
+                    </el-row>
+                    <Package_list />
+                </el-col>
+            </el-row>
+        </el-main>
+        </el-container>
+  </div>
+</template>
+
+<script>
+import Package_list from './components/Package_list.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Package_list
+  }
+}
+</script>
+
+<style lang="scss">
+body, html {
+    margin: 0;
+    padding: 0;
+}
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin: 0;
+    padding: 0;
+}
+
+.m-navbar {
+    background-color: white;
+    color: red($color: #671cc9);
+    height: 50px;
+}
+
+.m-content {
+    padding: 30px;
+    background-color: #efefef;
+}
+
+.m-subcontent {
+    padding: 30px;
+    background-color: #ffffff;
+}
+
+.image {
+    width: 100%;
+    height: 200px;
+    background: #efefef;
+    margin-bottom: 20px;
+}
+
+.m-side-detail {
+    font-size: 18px;
+}
+
+.m-tag {
+    background: #696969;
+    color: #ffffff;
+    border: none;
+}
+
+
+</style>
---
+++ front-end/src/components/Package_list.vue
@@ -0,0 +1,444 @@
+<template>
+    <div>        
+        <el-row :gutter="12">
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row >
+                        <el-col align="center">
+                            เริ่มต้น
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            0 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">ฟรีตลอดไป</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col>
+                            50 ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            0 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row>
+                        <el-col align="center">
+                            ร้านขนาดกลาง
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            450 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">1,200 บาท / 3 เดือน</el-tag>
+                            <el-tag type="info" class="m-tag">4,700 บาท / 1 เดือน</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col align="center">
+                            <el-button round>Buy</el-button>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col>
+                            50 ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            4 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row>
+                        <el-col align="center">
+                            ร้านขนาดใหญ่
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            650 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">1,800 บาท / 3 เดือน</el-tag>
+                            <el-tag type="info" class="m-tag">6,900 บาท / 1 เดือน</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col align="center">
+                            <el-button round>Buy</el-button>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            6 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            8 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row>
+                        <el-col align="center">
+                            ไม่จำกัด
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            850 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">6,900 บาท / 3 เดือน</el-tag>
+                            <el-tag type="info" class="m-tag">8,900 บาท / 1 เดือน</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col align="center">
+                            <el-button round>Buy</el-button>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            10 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            10 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+        </el-row>
+        <!-- Pro -->
+        <el-row :gutter="12">
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row >
+                        <el-col align="center">
+                            Quite Pro
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            890 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">2,600 บาท / 3 เดือน</el-tag>
+                            <el-tag type="info" class="m-tag">8,900 บาท / 1 เดือน</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            10 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            0 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row>
+                        <el-col align="center">
+                            Pro
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            1190 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">3,400 บาท / 3 เดือน</el-tag>
+                            <el-tag type="info" class="m-tag">11,900 บาท / 1 เดือน</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col align="center">
+                            <el-button round>Buy</el-button>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            15 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            2 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            15 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row>
+                        <el-col align="center">
+                            Truely Pro
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            1,590 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">1800 บาท / 3 เดือน</el-tag>
+                            <el-tag type="info" class="m-tag">6900 บาท / 1 เดือน</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col align="center">
+                            <el-button round>Buy</el-button>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col>
+                            300 ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            6 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            8 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="6" :lg="6">
+                <el-card shadow="never">
+                    <el-row>
+                        <el-col align="center">
+                            ไม่จำกัด
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            850 ฿
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col align="center">
+                            <el-tag type="info" class="m-tag">6900 บาท / 3 เดือน</el-tag>
+                            <el-tag type="info" class="m-tag">8900 บาท / 1 เดือน</el-tag>
+                        </el-col>
+                    </el-row>   
+                    <el-row>
+                        <el-col align="center">
+                            <el-button round>Buy</el-button>
+                        </el-col>
+                    </el-row>
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด ออเดอร์ / เดือน
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            10 Admin
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            1 คลังสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            10 ตัวแทนจำหน่าย
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                    <el-row>
+                        <el-col>
+                            ไม่จำกัด รายการสินค้า
+                        </el-col>
+                    </el-row>
+                    <hr class="m-line">
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Package_list'    
+}
+</script>
